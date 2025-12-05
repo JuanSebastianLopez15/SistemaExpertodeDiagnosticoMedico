ace2307895aa8ef9813d8b75c42e2aa8cbf78be5
luisaAran <luisamariaarangolopez@gmail.com>
2025-12-05 14:59:11 -0500

    Commit inicial

---
+++ src/main/java/luisaaran/sistema_experto/Sistema_experto.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package luisaaran.sistema_experto;
+
+/**
+ *
+ * @author luisaAran
+ */
+public class Sistema_experto {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
